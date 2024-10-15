94b2ac27b0dbde6d9cf3ffa4eaefb649e8d1a3ed
1013115 <108274782+1013115@users.noreply.github.com>
2024-10-15 16:12:09 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,19 @@
+import sys
+
+from itertools import combinations
+
+while(True):
+    
+    knum = list(map(int,input().split()))
+    k = knum[0]
+    if(k ==0): break
+    knum.pop(0)
+    
+
+    
+   # knum.sort()
+    kre = combinations(knum, 6)
+    
+    for i in kre:
+        print(*i)
+    print()
